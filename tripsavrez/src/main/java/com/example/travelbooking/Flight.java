c088053c4c9f9a7d0bf14aa4635d3f9ba80b9000
geeteshgautam <155235815+geeteshgautam@users.noreply.github.com>
2024-08-08 07:30:26 +0530

    Add files via upload

---
+++ tripsavrez/src/main/java/com/example/travelbooking/Flight.java
@@ -0,0 +1,5 @@
+package src.main.java.com.example.travelbooking;
+
+public class Flight {
+
+}
---
+++ tripsavrez/src/main/java/com/example/travelbooking/FlightService.java
@@ -0,0 +1,21 @@
+package com.example.travelbooking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlightService {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    public List<Flight> getAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    public Flight getFlightById(Long id) {
+        return flightRepository.findById(id).orElse(null);
+    }
+}
