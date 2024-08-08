a91c36120e5a6bd9933315d5c18b6d6aa44b0095
geeteshgautam <155235815+geeteshgautam@users.noreply.github.com>
2024-08-08 07:51:36 +0530

    Update Flights.java

---
+++ tripsavrez/src/main/java/com/example/travelbooking/Flights.java
@@ -0,0 +1,20 @@
+package main.java.com.example.travelbooking;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Flight {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String flightNumber;
+    private String origin;
+    private String destination;
+    private double price;
+
+    // Constructors, getters, setters
+}
