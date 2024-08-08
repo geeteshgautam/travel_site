b6a3a610b1eeac0b1f34b50f0d156bbdcc1c2321
geeteshgautam <155235815+geeteshgautam@users.noreply.github.com>
2024-08-08 07:52:39 +0530

    Update Service.java

--- tripsavrez/src/main/java/com/example/travelbooking/Service.java
+++ tripsavrez/src/main/java/com/example/travelbooking/Service.java
@@ -1,5 +1,29 @@
 package src.main.java.com.example.travelbooking;
+package src.main.java.com.example.travelbooking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlightService {
+
+    @Autowired
+    private FlightRepository flightRepository;
+
+    public List<Flight> getAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    public Flight getFlightById(Long id) {
+        return flightRepository.findById(id).orElse(null);
+    }
+}
+
+public @interface Service {
+
+}
 public @interface Service {
 
 }
